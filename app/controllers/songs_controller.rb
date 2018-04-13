f0b017321559ef32fa76567b2a3b0865d49c34b0
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-13 15:07:22 -0400

    lab completed

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+require "pry"
 class SongsController < ApplicationController
   def index
     @songs = Song.all
@@ -9,10 +10,21 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @genre = Genre.new
+    @artist = Artist.new
   end
 
   def create
     @song = Song.new(song_params)
+    genre = Genre.find(song_genre_id_param)
+    @song.genre = genre
+
+    song_notes_params.each_value do |note|
+      if note != ""
+        n = Note.create(content: note)
+        @song.notes << n
+      end
+    end
 
     if @song.save
       redirect_to @song
@@ -22,13 +34,22 @@ class SongsController < ApplicationController
   end
 
   def edit
+    print params
     @song = Song.find(params[:id])
+    @genre = Genre.new
+    @artist = Artist.new
   end
 
   def update
     @song = Song.find(params[:id])
 
     @song.update(song_params)
+    @song.genre = Genre.find(song_genre_id_param)
+
+    if song_notes_params[:song_notes_1] != "" && song_notes_params[:song_notes_2] != ""
+      @song.notes_1 = song_notes_params[:song_notes_1]
+      @song.notes_2 = song_notes_params[:song_notes_2]
+    end
 
     if @song.save
       redirect_to @song
@@ -47,7 +68,14 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name)
+  end
+
+  def song_genre_id_param
+    params.require(:song).permit(:song_genre_id)[:song_genre_id]
+  end
+
+  def song_notes_params
+    params.require(:song).permit(:notes_1, :notes_2)
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,58 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    if self.genre
+      self.genre.name
+    else
+      nil
+    end
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def notes_1=(note)
+    @notes ||= []
+    @notes << note
+  end
+
+  def notes_1
+
+  end
+
+  def notes_2=(note)
+    @notes ||= []
+    @notes << note
+  end
+
+  def notes_2
+
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      self.notes << Note.create(content: note)
+    end
+  end
+
+  def note_contents
+    self.notes.collect { |note_instance| note_instance.content}.select {|note| note != ""}
+
+  end
+
+end
